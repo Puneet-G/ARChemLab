cc275e8ac00d2ae341d6b97cf1b3913f3dca0c2e
Puneet <EngineerP@users.noreply.github.com>
2020-03-23 02:14:16 -0400

    tuned flame reactions

--- Assets/ChemLab/Scripts/FlameTestScript/FlameReaction.cs
+++ Assets/ChemLab/Scripts/FlameTestScript/FlameReaction.cs
@@ -9,7 +9,7 @@ public class FlameReaction : MonoBehaviour
 
     void OnStart()
     {
-        flame.GetComponent<ParticleSystem>().startColor = new Color(184f/255f, 159f/255f, 81f/255f,1f);
+        flame.GetComponent<ParticleSystem>().startColor = new Color(32f / 255f, 40f / 255f, 255f / 255f, 1f);
 
     }
 
@@ -23,23 +23,23 @@ public class FlameReaction : MonoBehaviour
                 flame.GetComponent<ParticleSystem>().startColor = new Color(.75f, 0f, 0f);
                 break;
             case "BaSalt":
-                flame.GetComponent<ParticleSystem>().startColor = new Color(.553f, .714f, 0f);
+                flame.GetComponent<ParticleSystem>().startColor = new Color(0.06666664f, 0.7090117f, 0.7960784f);
                 break;
             case "CuSalt":
-                flame.GetComponent<ParticleSystem>().startColor = new Color(.067f, .392f, .796f);
+                flame.GetComponent<ParticleSystem>().startColor = new Color(0.06666664f, 0.7960784f, 0.271321f);
                 break;
             case "KSalt":
-                flame.GetComponent<ParticleSystem>().startColor = new Color(200f / 255f, 162f / 255f, 200f / 255f);
+                flame.GetComponent<ParticleSystem>().startColor = new Color(224f / 255f, 33f / 255f, 224f / 255f);
                 break;
             default:
-                flame.GetComponent<ParticleSystem>().startColor = new Color(184f / 255f, 159f / 255f, 81f / 255f, 1f);
+                flame.GetComponent<ParticleSystem>().startColor = new Color(32f / 255f, 40f / 255f, 255f / 255f, 1f);
                 break;
         }
     }
 
     void OnTriggerExit(Collider collider)
     {
-        flame.GetComponent<ParticleSystem>().startColor = new Color(184f / 255f, 159f / 255f, 81f / 255f, 1f);
+        flame.GetComponent<ParticleSystem>().startColor = new Color(32f / 255f, 40f / 255f, 255f / 255f, 1f);
 
     }
 }
